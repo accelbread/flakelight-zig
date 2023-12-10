65604e0b6a10863713147e30ecf762b57a3bf823
Archit Gupta <archit@accelbread.com>
2023-12-10 13:57:22 -0800

    Update template

--- templates/zig-bin/flake.nix
+++ templates/zig-bin/flake.nix
@@ -1,16 +1,14 @@
 {
+  description = "Template Zig application.";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flakelight.url = "github:accelbread/flakelight";
     flakelight-zig.url = "github:accelbread/flakelight-zig";
   };
-  outputs = { flakelight, flakelight-zig, ... }@inputs: flakelight ./. {
-    imports = [ flakelight-zig.flakelightModules.default ];
-    inherit inputs;
-
-    name = "hello-world";
-    version = "0.0.1";
-    description = "Template Zig application.";
-    license = "agpl3Plus";
-  };
+  outputs = { flakelight-zig, ... }@inputs:
+    flakelight-zig ./. {
+      inherit inputs;
+      name = "hello-world";
+      version = "0.0.1";
+      license = "AGPL-3.0-or-later";
+    };
 }
